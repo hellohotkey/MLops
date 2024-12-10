e942033a367004e7b241f232c55d7ced9fb3c3d3
Duckkyung Han <129307893+hellohotkey@users.noreply.github.com>
2024-12-10 17:50:32 +0900

    Update and rename main.py to fastapi.py

--- fastapi.py
+++ fastapi.py
@@ -1,18 +1,19 @@
 import os
 import pickle
-
 from pydantic import BaseModel, conlist
 from typing import List
 from fastapi import FastAPI, Body
 
+# Load model and trains
 with open("model.pkl", "rb") as f:
-        model = pickle.load(f)
+    model = pickle.load(f)
+with open("trains.pkl", "rb") as f:
+    trains = pickle.load(f)
 
 class Dataset(BaseModel):
     data: List
 
 app = FastAPI()
-
 
 @app.post("/predict")
 def get_prediction(dataset: Dataset):
@@ -20,7 +21,6 @@ def get_prediction(dataset: Dataset):
     prediction = model.predict(data).tolist()
     log_proba = model.predict_proba(data).tolist()
     return {"prediction": prediction, "log_proba": log_proba}
-
 
 if __name__ == "__main__":
     print("test")
